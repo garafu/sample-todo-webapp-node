78e55f02b68e49b4d7e2aa9662b06c6a1ac496fc
Akinari Tsugo <garafo@gmail.com>
2024-08-14 17:47:15 +0900

    docker compose の初期化処理見直し

--- app/build.sh
+++ app/build.sh
@@ -2,15 +2,12 @@
 
 # Install dependencies
 apt-get update
-apt-get install -y curl netcat mysql-client
+apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
 
 # Install the application
 npm ci
-
-# Copy the configuration file
-cp /app/data/client.cnf /etc/mysql/my.cnf
 
 # Make the entrypoint script executable
 chmod +x /app/entrypoint.sh
